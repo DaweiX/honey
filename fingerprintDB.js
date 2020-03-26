03896d97315c9c9d781315501bf1663728d0d6b0
DaweiX <DaweiX@outlook.com>
2020-03-26 10:18:20 +0800

    Add files via upload

---
+++ fingerprintDB.js
@@ -0,0 +1,48 @@
+var db = [
+	{
+		'name': '彩云小译',
+		'id': 'jmpepeebcbihafjjadogphmbgiffiajh',
+		'f': ["http://caiyunapp.com/xiaoyi/web_translate_data_stat.html",
+				"https://www.caiyunapp.com/imgs/webtrs/default.png",
+				"https://caiyunapp.com/imgs/webtrs/fanyi-btn-hover.png",
+				"https://caiyunapp.com/imgs/webtrs/favorite-btn.png",
+				"https://caiyunapp.com/imgs/webtrs/left-slide.png",
+				"https://caiyunapp.com/imgs/xiaoyilogo.jpg",
+				"https://caiyunapp.com/images/favour.png",
+				"https://caiyunapp.com/imgs/webtrs/right-slide.png"]
+	},
+	{
+		'name':'Meet Me Dolby Voice 1.1',
+		'id':'bpdjhejefncgnahbpolfhcjkodmjlfaj',
+		'f':[
+				{
+					"MeetMeDolbyVoiceMsgP1x1": "log_msg",
+				 	"raw_value" : {
+					 	"component": "ChromeExt-FG", 
+					 	"message": "Sending 'ping' message to transport layer"
+				 	}
+				 },
+				 {
+				 	"MeetMeDolbyVoiceMsgP1x1":"ping"
+				 }
+			]
+	},
+
+	{
+		'name':'Dolby Voice 1.3',
+		'id': 'dpdceimakekmhakeipnflcgolggpncgd',
+		'f':[
+				{
+					"DolbyVoiceMsgP1x3": "log_msg",
+					"raw_value": {
+						"component": "ChromeExt-FG", 
+						"message": "Sending 'ping' message to transport layer"
+					}
+				},
+				{
+					"DolbyVoiceMsgP1x3":"ping"
+				}
+		]
+	}
+	
+]
---
+++ utils.js
@@ -0,0 +1,8 @@
+function isInArray(arr,value){
+    for(var i = 0; i < arr.length; i++){
+        if(value === arr[i]){
+            return true;
+        }
+    }
+    return false;
+}
